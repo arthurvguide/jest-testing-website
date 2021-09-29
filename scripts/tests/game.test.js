191f8e965f2e38131a35242bdfa332ccc4eef8d4
arthurvguide <arthurvguideuk@gmail.com>
2021-09-29 15:52:06 +0000

    Add and test showScore functions

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
 /**
  * @jest-environment jsdom
  */
-const { game } = require("..//game");
+const { game, newGame, showScore} = require("..//game");
 beforeAll( () => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -23,5 +23,30 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    })
+});
+
+describe("new game works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.currentGame = [1, 2, 3, 4];
+        game.playerMoves = [1, 2, 3, 4, 5];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to 0", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear currentGame", () => {
+        expect(game.currentGame).toEqual([]);
+    });
+    test("should clear playerMoves", () => {
+        expect(game.playerMoves).toEqual([]);
+    });
+    test("should display 0 to element with id score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
